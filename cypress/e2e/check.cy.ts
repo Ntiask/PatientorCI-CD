94454b4787f9d81dab959eb45c94bd6ce1f2dc35
Niko <niko.tiaskorpi@gmail.com>
2022-08-17 14:44:36 +0300

    added distribution to express.js of build

--- cypress/e2e/check.cy.ts
+++ cypress/e2e/check.cy.ts
@@ -1,6 +1,6 @@
 describe('check if opened', () => {
   it('passes', () => {
-    cy.visit('http://localhost:3000/');
+    cy.visit('http://localhost:3001/');
     cy.contains('Patientor');
     console.log('testi');
   });
